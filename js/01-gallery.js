31825a5ee1bafc69d5a8be579af7e321227b7d63
color <95304946+HermanMazur@users.noreply.github.com>
2022-06-08 13:25:36 +0300

    7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,30 +1,28 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const listGalleryItems = document.querySelector(".gallery");
+const markup = createGalleryMarkup(galleryItems);
 
-const listGalleryItems = document.querySelector('.gallery');
+listGalleryItems.insertAdjacentHTML("afterbegin", markup);
+listGalleryItems / addEventListener('click', onGalleryItemsClick);
 
-const markup = createGalleyItems(galleryItems);
-
-listGalleryItems.insertAdjacentHTML('afterbegin', markup);
-
-
-function createGalleyItems(galleryItems) {
-    return galleryItems.map(({ preview, original, description }) => {
-        `
-<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
+function createGalleryMarkup(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+        <div class="gallery__item">
+<a class="gallery__link" href="${original}">
     <img
     class="gallery__image"
     src="${preview}"
     data-source="${original}"
     alt="${description}"
     />
-    </a>
+</a>
 </div>
-    `;
-    })
-        .join("");
-
+`;
+		})
+		.join("");
 }
 
